d2961fa9e5d8465bf9c4f7f649872a27c501a756
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2025-03-13 18:37:01 +0100

    Tierklasse: Getter und Setter, Tierklasse ist abstrakt, weitere Atribute festgelegt

--- Tier.java
+++ Tier.java
@@ -2,9 +2,65 @@
  * Tier-Klasse - Damit werden alle Tiere erstellt
  *
  */
-public class Tier {
-    private String name;
-    private int level;
-    private int strenght;
-    private int xp_amount;
+public abstract class Tier { //Abstract => Klasse kann nicht instanziert werden
+    private String name; //Name des Tieres
+    private int level; //Level
+    private int strenght; //stärke des Tieres
+    private int xp_amount; //Menge an XP (für Levelup ggf)
+    private boolean enemie; //Gegner oder Spieler
+    private String entity_type;
+
+    /**
+     * Konstruktor mit Parametern
+     * @param name - Names des Tiers
+     * @param strenght - Stärke als GZ
+     * @param enemie - true = Gegner, false = kein Gegner
+     * @param entity_type - Tierart als String
+     */
+    public Tier (String name, int strenght, boolean enemie, String entity_type) {
+        this.name = name;
+        this.strenght = strenght;
+        this.enemie = enemie;
+        this.entity_type = entity_type;
+    }
+
+    /**
+     * gibt den Namen des Tiers zurück
+     * @return - name
+     */
+    public String getName () {
+        return this.name;
+    }
+
+    /**
+     * setzt den Namen (z.b. zum ändern)
+     * @param name - name
+     */
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    /**
+     * gibt die Kampfkraft/Stärke zurück
+     * @return - Kampfkraft
+     */
+    public int getStrenght () {
+        return this.strenght;
+    }
+
+    /**
+     * gibt zurück ob das Tier ein Gegner oder kein Gegener ist
+     * @return - true = Gegener, false = kein Gegner (Spieler)
+     */
+    public boolean isEnemie () {
+        return this.enemie;
+    }
+
+    /**
+     * gibt die Tierart als Text zurück
+     * @return - Tierart
+     */
+    public String getEntity_type () {
+        return this.entity_type;
+    }
 }
