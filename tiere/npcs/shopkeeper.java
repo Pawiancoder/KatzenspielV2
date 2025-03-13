295a960d69945c4e4d5530d334529807ac721522
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2025-03-13 21:07:47 +0100

    shopkeeper erweitert // Inventar erweitert

--- tiere/npcs/shopkeeper.java
+++ tiere/npcs/shopkeeper.java
@@ -2,7 +2,6 @@ package tiere.npcs;
 
 import tiere.Tier;
 import utils.ShopItem;
-
 import java.util.ArrayList;
 
 public class shopkeeper extends Tier {
@@ -16,7 +15,22 @@ public class shopkeeper extends Tier {
      * @param enemie      - true = Gegner, false = kein Gegner
      * @param entity_type - Tierart als String
      */
-    public shopkeeper(String name, int strenght, boolean enemie, String entity_type) {
+    public shopkeeper(String name, int strenght, boolean enemie, String entity_type, String shop_type, ArrayList<ShopItem> itemListe) {
         super(name, strenght, enemie, entity_type);
+        this.shop_type = shop_type;
+        this.itemListe = itemListe;
+    }
+
+    public String getShop_type () {
+        return this.shop_type;
+    }
+
+    @Override
+    public String toString () {
+        String text = "Preisliste: \n \n";
+        for (ShopItem item : itemListe) {
+            text += "- " + item.getItemName() + " - " + item.getPrice() + "Katzenmünzen\n";
+        }
+        return text;
     }
 }
--- utils/ShopItem.java
+++ utils/ShopItem.java
@@ -4,4 +4,23 @@ public class ShopItem {
     private String itemName;
     private int value;
     private int price;
+
+
+    public ShopItem (String itemName, int value, int price) {
+        this.itemName = itemName;
+        this.value = value;
+        this.price = price;
+    }
+
+    public String getItemName () {
+        return this.itemName;
+    }
+
+    public int getValue () {
+        return this.value;
+    }
+
+    public int getPrice () {
+        return this.price;
+    }
 }
