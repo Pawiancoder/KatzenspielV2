766ca7d4f70ab94693cf905326409233442fc31f
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2025-03-13 20:07:30 +0100

    Inventory Fix
    Co-Authored-By: Nico Rausch <172619610+Augenringe@users.noreply.github.com>

--- Inventar.java
+++ Inventar.java
@@ -7,12 +7,41 @@ public class Inventar {
     private Item[] slots;
     private int size;
 
-    public Inventory(int size) {
+    public void Inventory(int size) {
        this.size = size;
        this.slots = new Item[size];
+    }
     
-    public boolean 
+    public boolean addItem(Item item) {
+        for(int i = 0; i < size; i++) {
+            if (slots[i] == null) {
+                slots[i] = item;
+                return true;
 
+            }
+        }
+        System.out.println("Dein Inventar ist voll, Item wurde nicht Hinzugefügt! Kauf dir mehr Item Plätze Bozzo");
+        return false; //Inventar voll >.<
     }
 
-}
+    public boolean removeItem(int slot) {
+        if (slot >= 0 && slot < size && slots[slot] != null) {
+            slots[slot] = null;
+            return true;
+        }
+        return false; //Nicht vorhandener Slot || Slot schon leer
+        }
+
+        public Item getItem (int slot) {
+
+            return new Item();
+        }
+    }
+
+
+            
+        
+
+    
+
+
---
+++ Maus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Maksymilian
+ */
+public class Maus extends Tier{
+
+    public Maus (String name, int strenght, boolean enemie, String entity_type) {
+        super(name, strenght, enemie, entity_type);
+    }
+
+}
