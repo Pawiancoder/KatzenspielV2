a8bc528d7566c4e2921ab79ce36ef9c59c582958
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2025-03-13 18:27:02 +0100

    Basic Setup in der Tier-Klasse, Anfang Javadoc

--- Tier.java
+++ Tier.java
@@ -1,3 +1,10 @@
+/**
+ * Tier-Klasse - Damit werden alle Tiere erstellt
+ *
+ */
 public class Tier {
     private String name;
+    private int level;
+    private int strenght;
+    private int xp_amount;
 }
